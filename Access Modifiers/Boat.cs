2bf4019c6557bfbe69612098d0d43d80e0e6dd67
CheriaW <cheria.white@yahoo.com>
2017-10-10 13:06:38 -0400

    access modifiers end

---
+++ Access Modifiers/Boat.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Access_Modifiers
+{
+    class Boat
+    {
+        //fields- used to be modified through the program. values to be used the program. put behind properties
+        public int numberOfPassengers;
+
+        private string name;
+        private double engineSize;
+
+            public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        //default constructor
+        public Boat()
+        {
+
+        }
+        public Boat(int numberOfPassengers, string name, double engineSize)
+        {
+            this.numberOfPassengers = numberOfPassengers;
+            this.name = name;
+            this.engineSize = engineSize;
+        }
+
+        //overloaded constructors
+        public void Move()//method (can be accessed from anywhere in the program)
+        {
+            //code to make boat move
+            Console.WriteLine(name + " moves forward.");
+        }
+        private double CalculateMPG()//method (can only be accessed in this part of the program)
+        {
+            //code to calculate MPG
+            double MPG = engineSize * .45d;
+            return MPG;
+        }
+
+        public void BoatInfo()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Passengers: " + numberOfPassengers);
+            Console.WriteLine("Engine size: " + engineSize);
+            Console.WriteLine("MPG: " + CalculateMPG());
+            Console.WriteLine("");
+        }
+    }
+}
--- Access Modifiers/Program.cs
+++ Access Modifiers/Program.cs
@@ -10,6 +10,19 @@ namespace Access_Modifiers
     {
         static void Main(string[] args)
         {
+            //class //variable     //get info from the inside of (from constructors from "Boat" class)
+            Boat ssWetDude = new Boat(3, "SS Wet Dude", 18.6);
+            ssWetDude.Move();
+
+
+            //we can access these feilds because theyre public
+            //we dont see engineSize or calculateMPG when typing in the field names (as suggestions)
+            //program doesnt see that they even exist. why?
+
+            ssWetDude.BoatInfo();
+
+            Boat ssWetMonkey = new Boat(12, "SS Wet Monkey", 48.2);
+            ssWetMonkey.BoatInfo();
         }
     }
 }
